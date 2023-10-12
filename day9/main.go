9641bf7d81afaa3a335a977d2c5545044ba3bcf6
Szymon Gazda <szymongazda95@gmail.com>
2023-10-12 16:06:15 +0200

    refactor(day9): Adjust implementation and solve part2

--- day9/main.go
+++ day9/main.go
@@ -24,49 +24,122 @@ func getDirection(direction string) (int, int) {
 	}
 }
 
-func getDistance(h, t Position) int {
+type Position [2]int
+
+func (p Position) getAbsDistTo(to Position) (int, int) {
 	var diffY int
 	var diffX int
-	hy := float64(h[0])
-	hx := float64(h[1])
-	ty := float64(t[0])
-	tx := float64(t[1])
+	hy := float64(p[0])
+	hx := float64(p[1])
+	ty := float64(to[0])
+	tx := float64(to[1])
 
-	if h[0] < t[0] {
+	if p[0] < to[0] {
 		diffY = int(math.Abs(ty - hy))
 	} else {
 		diffY = int(math.Abs(hy - ty))
 	}
-	if h[1] < t[1] {
+	if p[1] < to[1] {
 		diffX = int(math.Abs(tx - hx))
 	} else {
 		diffX = int(math.Abs(hx - tx))
 	}
-
-	if diffX > diffY {
-		return diffX
-	} else {
-		return diffY
-	}
+	return diffY, diffX
 }
 
-func move(h, t Position, direction string) (Position, Position) {
-	y, x := getDirection(direction)
-	var nextHead Position
-	nextHead[0] = h[0] + y
-	nextHead[1] = h[1] + x
+func (p Position) getOffsetTo(to Position) (int, int) {
+	y, x := p.getAbsDistTo(to)
+	if p[0] < to[0] {
+		y = y * -1
+	}
+	if p[1] < to[1] {
+		x = x * -1
+	}
+	return y, x
+}
 
-	isAdjacent := getDistance(nextHead, t) < 2
+type Knot struct {
+	id           int
+	next         *Knot
+	prev         *Knot
+	position     Position
+	prevPosition Position
+}
+
+func (r *Knot) move(direction string) {
+	y, x := getDirection(direction)
+	var nextPosition Position
+	nextPosition[0] = r.position[0] + y
+	nextPosition[1] = r.position[1] + x
+	r.prevPosition = r.position
+	r.position = nextPosition
+}
+
+func (r *Knot) follow() {
+	prevKnotMoveY, prevKnotMoveX := r.prev.position.getOffsetTo(r.prev.prevPosition)
+	hasPrevKnotMovedDiagonaly := prevKnotMoveY != 0 && prevKnotMoveX != 0
+
+	offsetY, offsetX := r.prev.position.getOffsetTo(r.position)
+	distY, distX := r.position.getAbsDistTo(r.prev.position)
+	isAdjacent := distY < 2 && distX < 2
+
+	willBeSameRow := offsetY == 0
+	willBeSameCol := offsetX == 0
+
+	var nextPosition Position
 
 	if isAdjacent {
-		return nextHead, t
+		return
+	} else if hasPrevKnotMovedDiagonaly && willBeSameCol {
+		nextPosition[1] = r.position[1]
+		if r.prev.position[0] > r.position[0] {
+			nextPosition[0] = r.prev.position[0] - 1
+		} else {
+			nextPosition[0] = r.prev.position[0] + 1
+		}
+	} else if hasPrevKnotMovedDiagonaly && willBeSameRow {
+		nextPosition[0] = r.position[0]
+		if r.prev.position[1] > r.position[1] {
+			nextPosition[1] = r.prev.position[1] - 1
+		} else {
+			nextPosition[1] = r.prev.position[1] + 1
+		}
+	} else if hasPrevKnotMovedDiagonaly {
+		nextPosition[0] = r.position[0] + prevKnotMoveY
+		nextPosition[1] = r.position[1] + prevKnotMoveX
 	} else {
-		nextTail := h
-		return nextHead, nextTail
+		nextPosition[0] = r.prev.prevPosition[0]
+		nextPosition[1] = r.prev.prevPosition[1]
+	}
+	r.prevPosition = r.position
+	r.position = nextPosition
+}
+
+const (
+	initialY = 0
+	initialX = 0
+)
+
+func newKnot(id int, p Position, prev, next *Knot) *Knot {
+	return &Knot{
+		id:           id,
+		next:         next,
+		prev:         prev,
+		position:     p,
+		prevPosition: p,
 	}
 }
 
-type Position [2]int
+func prepare(count int, startingPosition Position) *Knot {
+	head := newKnot(0, startingPosition, nil, nil)
+	curr := head
+	for i := 0; i < count-1; i++ {
+		nextKnot := newKnot(i+1, startingPosition, curr, nil)
+		curr.next = nextKnot
+		curr = nextKnot
+	}
+	return head
+}
 
 func main() {
 	f, err := os.ReadFile("input.txt")
@@ -75,9 +148,11 @@ func main() {
 	}
 	lines := strings.Split(string(f), "\n")
 
-	head := Position{0, 0}
-	tail := Position{0, 0}
-	visitedFields := map[string]int{"0:0": 1}
+	head1 := prepare(2, Position{15, 11})
+	head2 := prepare(10, Position{15, 11})
+
+	tail1visited := make(map[Position]bool)
+	tail2visited := make(map[Position]bool)
 
 	for _, l := range lines {
 		instruction := strings.Fields(l)
@@ -87,14 +162,70 @@ func main() {
 			log.Fatalf("Error converting steps to integer: %+v", err)
 		}
 
-		for i := 0; i < steps; i++ {
-			h, t := move(head, tail, direction)
-			head = h
-			tail = t
+		for m := 0; m < steps; m++ {
+			head1.move(direction)
+			curr := head1
+			for curr.next != nil {
+				curr = curr.next
+				curr.follow()
+				if curr.next == nil {
+					tail1visited[curr.position] = true
+				}
+			}
 
-			key := fmt.Sprintf("%d:%d", t[0], t[1])
-			visitedFields[key]++
+			head2.move(direction)
+			curr2 := head2
+
+			for curr2.next != nil {
+				curr2 = curr2.next
+				curr2.follow()
+				if curr2.next == nil {
+					tail2visited[curr2.position] = true
+				}
+			}
 		}
 	}
-	fmt.Printf("Part 1: %d\n", len(visitedFields))
+
+	fmt.Printf("Part 1: %d\n", len(tail1visited))
+	fmt.Printf("Part 2: %d\n", len(tail2visited))
+}
+
+// Utility that helps visualise the current state of the knots
+// w 26 h 22 for pt2
+func PrintCurrentState(head *Knot, width, height int) {
+	grid := make([][]string, height)
+	for i := 0; i < height; i++ {
+		row := make([]string, width)
+		for j := range row {
+			row[j] = "."
+		}
+		grid[i] = row
+	}
+	grid[initialY][initialX] = "s"
+
+	curr := head
+	for curr != nil {
+		y := curr.position[0]
+		x := curr.position[1]
+		if grid[y][x] == "." || grid[y][x] == "s" {
+			if curr.id == 0 {
+				grid[y][x] = "H"
+			} else if curr.id == 9 {
+				grid[y][x] = "t"
+			} else {
+				grid[y][x] = fmt.Sprintf("%d", curr.id)
+			}
+		}
+		curr = curr.next
+	}
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if x == width-1 {
+				fmt.Printf("%s\n", grid[y][x])
+			} else {
+				fmt.Printf("%s", grid[y][x])
+			}
+		}
+	}
 }
